bd88691b725753dbb05721fae286fbb0ee8cfa4c
Encho Petrov <encho.petrov@evrotrust.com>
2026-03-14 01:01:08 +0200

    removed bom from migrations

--- migrations/000024_fix_fortress_rewards.up.sql
+++ migrations/000024_fix_fortress_rewards.up.sql
@@ -1,4 +1,4 @@
-﻿UPDATE building_rewards SET reward_name = 'Pet Stones', reward_icon = 'pet_stone.png' WHERE id = 52;
+UPDATE building_rewards SET reward_name = 'Pet Stones', reward_icon = 'pet_stone.png' WHERE id = 52;
 UPDATE building_rewards SET reward_name = 'Enhancement XP Component', reward_icon = 'enhancement.png' WHERE id = 53;
 UPDATE building_rewards SET reward_name = 'Pet Stones', reward_icon = 'pet_stone.png' WHERE id = 56;
 DELETE FROM building_rewards WHERE id = 57;
